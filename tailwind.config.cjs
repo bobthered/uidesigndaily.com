5e8385cb3f5cdba8c70e7bf9346f1519c69b0f1b
Bob McAleavey <bobmcaleavey@gmail.com>
2021-10-20 13:57:59 -0400

    add - boxShadow theme extension

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,18 @@
 // lib
 const path = require('path');
+const defaultTheme = require('tailwindcss/defaultTheme')
+const boxShadow = require('@bobthered/svelte-tailwindcss-ui/tailwind/themeExtensions/boxShadow')
+
 const svelteTailwindCSSUIPath = require.resolve('@bobthered/svelte-tailwindcss-ui');
 const purgePath = path.join(svelteTailwindCSSUIPath, '../', '../', '**', '*.{html,js,svelte,js}');
+
+const makeShadow = (name, rgb) => {
+	let obj = {};
+
+	// obj[name] = `0 17px 17px -10px rgba(${rgb}, .4)`;
+
+	return obj;
+}
 
 const config = {
 	mode: 'jit',
@@ -16,7 +27,15 @@ const config = {
 	},
 
 	theme: {
+
+		colors: {
+			...defaultTheme.colors,
+		},
 		extend: {
+			boxShadow: theme => boxShadow(theme, makeShadow),
+			fontFamily: {
+				// sans: ['sans-serif']
+			}
 		}
 	},
 
