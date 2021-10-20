c60288645df6679d74a739696b9a65cc7205d5a1
Bob McAleavey <bobmcaleavey@gmail.com>
2021-10-20 16:09:52 -0400

    add - loading logic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,17 +5,38 @@
   
   // stores
   import theme from '@bobthered/svelte-tailwindcss-ui/theme';
+  theme.update(defaultTheme);
 
   // components
   import { Button } from '@bobthered/svelte-tailwindcss-ui';
 
   // props ( internal )
+  let loaded = false;
 
   // props ( dynamic )
+  $: tw = {
+    loading: {
+      alignItems: 'items-center',
+      backgroundColor: 'bg-white',
+      duration: 'duration-1000',
+      flex: 'flex',
+      justifyItems: 'justify-center',
+      height: 'h-screen',
+      inset: 'top-0 left-0',
+      opacity: loaded ? 'opacity-0' : 'opacity-1',
+      pointerEvents: loaded ? 'pointer-events-none' : 'pointer-events-auto',
+      position: 'fixed',
+      transform: 'transform',
+      transtion: 'transition',
+      scale: loaded ? 'scale-[1.1]' : 'scale-[1]',
+      width: 'w-screen',      
+    }
+  }
 
   // lifecycle
   onMount(async() => {
     theme.update(defaultTheme);
+    loaded = true;
   })
 </script>
 
@@ -25,4 +46,9 @@
 
 <div class="min-w-screen min-h-screen flex items-center justify-center">
 
+</div>
+
+<!-- loading spinner -->
+<div class={Object.values(tw.loading).join(' ')}>
+  <Spinner />
 </div>
